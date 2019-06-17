21e890e9e419c9be2593e8dc9d19bc21ed964d0c
michaelrambeau <mikeair@gmail.com>
2019-06-17 14:21:37 +0900

    Reorder class methods

--- package/src/window-size.js
+++ package/src/window-size.js
@@ -4,22 +4,15 @@ import throttle from 'lodash/throttle';
 
 export class WindowSize extends React.Component {
   static propTypes = {
-    throttleMs: PropTypes.number,
+    wait: PropTypes.number,
     children: PropTypes.func.isRequired
   };
 
   static defaultProps = {
-    throttleMs: 200
+    wait: 200
   };
 
   state = this.getWindowSize();
-
-  getWindowSize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    return {width, height};
-  }
 
   componentDidMount() {
     window.addEventListener('resize', this.handleResize);
@@ -29,12 +22,19 @@ export class WindowSize extends React.Component {
     window.removeEventListener('resize', this.handleResize);
   }
 
+  getWindowSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    return {width, height};
+  }
+
   setWindowSize = () => {
     const size = this.getWindowSize();
     this.setState(size);
   };
 
-  handleResize = throttle(this.setWindowSize, this.props.throttleMs);
+  handleResize = throttle(this.setWindowSize, this.props.wait);
 
   render() {
     return this.props.children({...this.state});
